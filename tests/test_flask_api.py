b8ee411d86f5f085280dc21716336fcc10477e40
MBDogar <murtazabilalqasim@gmail.com>
2025-05-30 21:54:35 +0000

    test update for api

--- tests/test_flask_api.py
+++ tests/test_flask_api.py
@@ -1,5 +1,10 @@
-import requests
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from app import app
 
 def test_api():
-    res = requests.post('http://localhost:5000/predict', data={'age': 25, 'weight': 70})
-    assert res.status_code == 200
+    client = app.test_client()
+    response = client.post('/predict', data={'age': 25, 'weight': 70})
+    assert response.status_code == 200
