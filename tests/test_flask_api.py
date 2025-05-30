683be44aeae681af204355d70b46536239d70b04
MBDogar <murtazabilalqasim@gmail.com>
2025-05-30 21:49:03 +0000

    revert changes for test API

--- tests/test_flask_api.py
+++ tests/test_flask_api.py
@@ -1,19 +1,5 @@
-from unittest.mock import patch
-import pytest
-
-@pytest.fixture(autouse=True)
-def mock_model_load():
-    with patch('app.joblib.load') as mock_load:
-        class DummyModel:
-            def predict(self, X):
-                return [800.857]
-        mock_load.return_value = DummyModel()
-        yield
-
-from app import app
+import requests
 
 def test_api():
-    client = app.test_client()
-    response = client.post('/predict', data={'age': 25, 'weight': 70})
-    assert response.status_code == 200
-    assert b"800.86" in response.data  # check if prediction rendered in response
+    res = requests.post('http://localhost:5000/predict', data={'age': 25, 'weight': 70})
+    assert res.status_code == 200
